57f2d268cd109287ffd258c321ad3d4466fc0c97
F0XZz <958828188@qq.com>
2021-09-07 14:14:53 +0800

    rewrite class

--- mulipleThread/threadClass.cpp
+++ mulipleThread/threadClass.cpp
@@ -1,28 +1,40 @@
-Mat MultipleThread::CustomThread(Mat src,int threadIndex){
+Mat CustomThread(Mat src,int threadIndex){
+
     thread MatThreadProcess[threadIndex];
+
     int srcHeight = src.size().height;
     int srcWidth = src.size().width;
     int cropHeight = srcHeight/threadIndex;
+
     Mat DstTemp;
+
     vector<Mat> dstThreadMat=vector<Mat>(threadIndex);
 
     Rect initRect(0,0,srcWidth,cropHeight);
     Point movePoint(0,cropHeight);
+
     //CREATE THE THREAD
     for (int i =0;i<threadIndex;++i){
-        MatThreadProcess[i]=thread(&colorFilter::ThreadUseColorRule,&cfProcess,src(initRect),std::ref(dstThreadMat.at(i)));
+        MatThreadProcess[i]=thread(&colorFilter::ThreadUseColorRule,&cfProcess,
+                                   src(initRect),std::ref(dstThreadMat.at(i)));
         initRect = initRect+movePoint;
     }
+
     //RUN THE THREAD
     for (auto& t: MatThreadProcess){
         t.join();
     }
+
     // MIX THE CROP IMAGE
     for(int j = 0;j<threadIndex-1;++j){
         if(j==threadIndex-2){
-            vconcat(dstThreadMat.at(j), dstThreadMat.at(j+1),DstTemp);
+            vconcat(dstThreadMat.at(j),
+                    dstThreadMat.at(j+1),
+                    DstTemp);
         }
-        vconcat(dstThreadMat.at(j), dstThreadMat.at(j+1),dstThreadMat.at(j+1));
+        vconcat(dstThreadMat.at(j),
+                dstThreadMat.at(j+1),
+                dstThreadMat.at(j+1));
     }
     return DstTemp;
 }
